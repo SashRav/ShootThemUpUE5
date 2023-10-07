a67d4d77d0bb1d69f493f7fbd030f02f2bc584fa
SashRav <sasha_ravinsky@tut.by>
2023-10-07 14:13:08 +0300

    Lesson 25. Added player movement using Enhanced Input subsystem.

--- Source/ShootThemUp/Public/Player/STUBaseCharacter.h
+++ Source/ShootThemUp/Public/Player/STUBaseCharacter.h
@@ -1,4 +1,4 @@
-// Shoot Them Up game, All Rights Reserved. 
+// Shoot Them Up game, All Rights Reserved.
 
 #pragma once
 
@@ -7,27 +7,40 @@
 #include "STUBaseCharacter.generated.h"
 
 class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
 
 UCLASS()
 class SHOOTTHEMUP_API ASTUBaseCharacter : public ACharacter
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
+
+    // Mapping Context for player movement
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+    UInputMappingContext* DefaultMappingContext;
+
+    // Input Action for player movement
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+    UInputAction* MoveAction;
 
 public:
-	// Sets default values for this character's properties
-	ASTUBaseCharacter();
+    // Sets default values for this character's properties
+    ASTUBaseCharacter();
 
 protected:
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
     UCameraComponent* CameraComponent;
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+    // Called when moving action is triggered
+    void Move(const FInputActionValue& Value);
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+public:
+    // Called every frame
+    // virtual void Tick(float DeltaTime) override;
 
+    // Called to bind functionality to input
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 };
